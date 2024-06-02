e0d47df6677cf247951bccd5f8f14e81e3d199c8
Balbino Moyano López <bmoylop0903@g.educaand.es>
2024-06-02 19:46:43 +0200

    hacer todo el seguimiento del pedido

--- src/main/java/com/shop/iesvdc/shop/controller/OrderTrackingController.java
+++ src/main/java/com/shop/iesvdc/shop/controller/OrderTrackingController.java
@@ -1,5 +1,10 @@
 package com.shop.iesvdc.shop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tracking")
 public class OrderTrackingController {
     
 }
